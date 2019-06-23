5b722cf23c8a72e001fd4ff5413a49d84a975c43
willianra <willianra@gmail.com>
2019-06-23 09:41:03 -0400

    layout added

--- app/controllers/consultors_controller.rb
+++ app/controllers/consultors_controller.rb
@@ -8,7 +8,7 @@ class ConsultorsController < ApplicationController
     respond_to do |format|
       format.html
       format.json
-      format.pdf { render template: 'consultors/reporte_consultores', pdf: 'reporte'}
+      format.pdf { render template: 'consultors/reporte_consultores', pdf: 'reporte',layout: 'pdf'}
     end
   end
 
